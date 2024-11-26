01c8d79be16f3f7bd2dcc70b22b79bea45084fd6
nabillahumi <nabillahumi003@gmail.com>
2024-11-26 08:40:04 +0700

    Fungsi 1 Jobsheet 11

--- Kafe18.java
+++ Kafe18.java
@@ -1,6 +1,16 @@
 public class Kafe18 {
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
+        Menu("Andi", true);
+    }
+
+    public static  void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + " ! ");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
